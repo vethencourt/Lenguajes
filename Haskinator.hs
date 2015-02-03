593b72f2b469a165370f57bb138b09a1e8bee387
vethencourt <ricardo.vethencourt.m@gmail.com>
2015-02-02 23:17:39 -0430

    para merge

--- Haskinator.hs
+++ Haskinator.hs
@@ -2,12 +2,12 @@ module Haskinator where
 import Oraculo
 
 main = do
-	putStrLn "Haskinator!"
+	putStrLn "¡Haskinator!"
 	putStrLn "Seleccione una opcion indicando su numero:"
 	putStrLn "(1) Crear oraculo nuevo."
-	putStrLn "(2) Predecir"
-	putStrLn "(3) Cargar"
-	putStrLn "(4) Consultar pregunta crucial"
+	putStrLn "(2) Predecir."
+	putStrLn "(3) Cargar."
+	putStrLn "(4) Consultar pregunta crucial."
 	opcion <- getLine
 
 {-	case opcion of 
@@ -23,7 +23,6 @@ crearOraculo = Nothing
 persistir :: String -> Maybe Oraculo -> IO ()
 persistir fileName (Just oraculo) = writeFile fileName (show oraculo) 
 persistir fileName Nothing = error "Oraculo vacio"
-
 
 --predecir :: Oraculo
 
